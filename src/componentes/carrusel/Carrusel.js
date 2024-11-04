3842d9e29481775b11f04a4541202b58898b624f
Skinnieraxis1 <skinnieraxis1lauf@gmail.com>
2024-11-04 04:28:03 -0300

    Ultimo Commit todo terminado

--- src/componentes/carrusel/Carrusel.js
+++ src/componentes/carrusel/Carrusel.js
@@ -64,42 +64,42 @@ function Carrusel() {
     <div className="slider-container">
       <Slider {...settings}>
         <div className="carrousel-card">
-          <img src="/img/carrousel-img-1.jpg" className="w-100"/>
+          <img src="https://acdn.mitiendanube.com/stores/026/096/themes/idea/1-slide-1728497127053-1652369950-2fb028603172d054736eb30151ad3e331728497151-1920-1920.jpg?264329645" className="w-100"/>
           <div className="carrousel-card-body">
             <h2 className="carrousel-card-title old-standard-tt-bold">Cartoon</h2>
             <buttom className="carrousel-card-button old-standard-tt-regular">more</buttom>
           </div>
         </div>
         <div className="carrousel-card">
-          <img src="/img/carrousel-img-2.jpg" className="w-100"/>
+          <img src="https://www.infobae.com/new-resizer/PVDFizroT_k2950kvztAznX2A-8=/arc-anglerfish-arc2-prod-infobae/public/YLBYHRHKWJCLBLUXLI4NUBXBCM.jpg" className="w-100"/>
           <div className="carrousel-card-body">
             <h2 className="carrousel-card-title old-standard-tt-bold">Rock</h2>
             <buttom className="carrousel-card-button old-standard-tt-regular">more</buttom>
           </div>
         </div>
         <div className="carrousel-card">
-          <img src="/img/carrousel-img-3.jpg" className="w-100"/>
+          <img src="https://images.squarespace-cdn.com/content/v1/547a3834e4b053a861c4874e/55fc228e-28de-491b-943b-3bf6a31580ff/Sustainably+Chic+%7C+Sustainable+Fashion+Blog+%7C+How+to+Create+Sustainable+Fall+Outfits+With+The+Clothes+You+already+Own+.jpg" className="w-100"/>
           <div className="carrousel-card-body">
             <h2 className="carrousel-card-title old-standard-tt-bold">Rustic</h2>
             <buttom className="carrousel-card-button old-standard-tt-regular">more</buttom>
           </div>
         </div>
         <div className="carrousel-card">
-          <img src="/img/carrousel-img-4.jpg" className="w-100"/>
+          <img src="https://cdn.britannica.com/34/201734-050-2B1ECD3E/Dominic-Monaghan-Merry-scene-Elijah-Wood-Frodo.jpg" className="w-100"/>
           <div className="carrousel-card-body">
             <h2 className="carrousel-card-title old-standard-tt-bold">Adventure</h2>
             <buttom className="carrousel-card-button old-standard-tt-regular">more</buttom>
           </div>
         </div>
         <div className="carrousel-card">
-          <img src="/img/carrousel-img-5.jpg" className="w-100"/>
+          <img src="https://cdn.costumewall.com/wp-content/uploads/2019/10/back-to-the-future-costumes.jpg" className="w-100"/>
           <div className="carrousel-card-body">
             <h2 className="carrousel-card-title old-standard-tt-bold">80'</h2>
             <buttom className="carrousel-card-button old-standard-tt-regular">more</buttom>
           </div>
         </div>
         <div className="carrousel-card">
-          <img src="/img/carrousel-img-6.jpg" className="w-100"/>
+          <img src="https://i0.wp.com/bamfstyle.com/wp-content/uploads/2024/10/PFVincentSuit-CL-Suit1.jpg?ssl=1" className="w-100"/>
           <div className="carrousel-card-body">
             <h2 className="carrousel-card-title old-standard-tt-bold">Serius</h2>
             <buttom className="carrousel-card-button old-standard-tt-regular">more</buttom>
